7c8bf9435444a442da87a25c164af9b87d936dcd
Melvin Musehani <melvinmusehani@Melvins-MacBook-Pro.local>
2016-06-25 14:20:55 +0200

    sample example

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -1,10 +1,17 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {SamplePage} from '../sample/sample';
 
 @Component({
   templateUrl: 'build/pages/about/about.html'
 })
 export class AboutPage {
   constructor(private navController: NavController) {
+    
   }
+
+  showSample() {
+    this.navController.push(SamplePage);
+  }
+
 }
